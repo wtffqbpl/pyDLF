c6505de2f205aa0fa004736885b179b88524e273
Yuanjun Ren <renyj@buaa.edu.cn>
2025-04-07 11:03:47 +0800

    Add cmake configurations

---
+++ tests/armadillo_test.cc
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <armadillo>
+#include <iostream>
+
+
+TEST(armadillo, basic_test) {
+  using namespace arma;
+
+  arma::fmat a1 =
+    "1, 2, 3;"
+    "4, 5, 6;"
+    "7, 8, 9;";
+
+  arma::fmat a2 = a1;
+
+  std::cout << a2 * a1 << std::endl;
+}
